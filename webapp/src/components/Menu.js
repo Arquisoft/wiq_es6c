ff8991023995470a5edff54845d24622be6ead30
Liliana <uo288574@uniovi.es>
2024-03-13 19:33:44 +0100

    sonarcloud problem solved at menu

--- webapp/src/components/Menu.js
+++ webapp/src/components/Menu.js
@@ -17,6 +17,12 @@ const DatosContext = React.createContext();
 
 var questions = []
 
+function secureRandomNumber(max) {
+  const randomBytes = new Uint32Array(1);
+  window.crypto.getRandomValues(randomBytes);
+  return randomBytes[0] % max;
+}
+
 function shuffleArray(array) {
   // Crea una copia del array original
   const shuffledArray = [...array];
@@ -24,7 +30,8 @@ function shuffleArray(array) {
   // Recorre el array desde el último elemento hasta el primero
   for (let i = shuffledArray.length - 1; i > 0; i--) {
     // Genera un índice aleatorio entre 0 y el índice actual
-    const randomIndex = Math.floor(Math.random() * (i + 1));
+    //const randomIndex = Math.floor(Math.random() * (i + 1));
+    const randomIndex = secureRandomNumber(i + 1);
 
     // Intercambia el elemento actual con el elemento del índice aleatorio
     const temp = shuffledArray[i];
