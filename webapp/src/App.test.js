6b1ecae1837aff8a966351c05247a138482c3c8c
alegarman2002 <uo283076@uniovi.es>
2024-03-13 15:53:57 +0100

    Fixed Login test, tryung to fix app test

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -6,11 +6,7 @@ import { Router } from 'react-router-dom';
 
 test('renders learn react link', () => {
   render(
-  <ContextFun>
-    <Router>
-      <App />
-    </Router>
-  </ContextFun>
+    <App />
   );
   const linkElement = screen.getByText(/Welcome to wiq_0/i);
   expect(linkElement).toBeInTheDocument();
