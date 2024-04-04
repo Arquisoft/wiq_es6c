0d4ccece80e1ede9ee2bb5733e0bcf26d6feafdf
Liliana <uo288574@uniovi.es>
2024-04-04 12:59:46 +0200

    remove import

--- webapp/src/components/Menu.test.js
+++ webapp/src/components/Menu.test.js
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react';
-import { ContextFun } from '../Context';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import { BrowserRouter as Router } from 'react-router-dom';
