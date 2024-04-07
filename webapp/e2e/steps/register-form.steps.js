21cbc9a4fa8554c2eb92ecbc0f8c1ec5ec42a9c4
Marco Quintana <UO287872@uniovi.es>
2024-04-07 20:05:47 +0200

    4th attempt........

--- webapp/e2e/steps/register-form.steps.js
+++ webapp/e2e/steps/register-form.steps.js
@@ -37,7 +37,7 @@ defineFeature(feature, test => {
     when('I fill the data in the form and press submit', async () => {
       await expect(page).toFill('input[name="username"]', username);
       await expect(page).toFill('input[name="password"]', password);
-      await expect(page).toClick('button', { text: 'Add User' })
+      await expect(page).toClick('button', { text: 'Add user' })
     });
 
     then('A confirmation message should be shown in the screen', async () => {
