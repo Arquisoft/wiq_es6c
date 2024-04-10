d47717f0a68ed2342d7ca7026e22ca604f206b37
Liliana <uo288574@uniovi.es>
2024-04-10 19:09:19 +0200

    new game mode (we have to refactor between game modes)

--- webapp/src/components/game/Game3.js
+++ webapp/src/components/game/Game3.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Typography, Box, LinearProgress} from '@mui/material';
-import './FirstGame.css';
 import 'react-circular-progressbar/dist/styles.css';
 import axios from 'axios';
 import { useLocation, useNavigate } from 'react-router-dom';
-import Button from './Button';
-import GoBackButton from './GoBackButton';
-import { Footer } from './footer/Footer';
-import { Nav } from './nav/Nav';
+import Button from '../Button';
+import GoBackButton from '../GoBackButton';
+import { Footer } from '../footer/Footer';
+import { Nav } from '../nav/Nav';
 
 var currentQuestionIndex = 0;
 
@@ -23,12 +22,10 @@ const Quiz = () => {
   var allQuestions = useLocation().state.questions;
 
   var id = useLocation().state.gameId;
-  console.log(id)
 
-  // const [currentQuestionIndex, setCurrentQuestionIndex] = useState(storedInt);
-  // const [isCorrect, setIsCorrect] = useState(false);
   const [remTime, setRemTime] = useState(0);
   const [numErrors, setNumErrors] = useState(3);
+
 
   useEffect(() => {
     const time = setInterval(() => {
@@ -37,7 +34,7 @@ const Quiz = () => {
           newQuestion();
           return 0; 
         }
-        const diff = crypto.getRandomValues(new Uint32Array(1))[0] % 11; // Generate a random number between 0 and 10
+        const diff = crypto.getRandomValues(new Uint32Array(1))[0] % 11; 
         return Math.min(progress + diff, 100);
       });
     }, 500);
@@ -47,9 +44,6 @@ const Quiz = () => {
     };
   });
 
-  const esperar = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  };
 
   function secureRandomNumber(max) {
     const randomBytes = new Uint32Array(1);
@@ -58,32 +52,43 @@ const Quiz = () => {
   }
 
   function shuffleArray(array) {
-    // Crea una copia del array original
     const shuffledArray = [...array];
   
-    // Recorre el array desde el último elemento hasta el primero
     for (let i = shuffledArray.length - 1; i > 0; i--) {
-      // Genera un índice aleatorio entre 0 y el índice actual
-      //const randomIndex = Math.floor(Math.random() * (i + 1));
       const randomIndex = secureRandomNumber(i + 1);
 
-      // Intercambia el elemento actual con el elemento del índice aleatorio
       const temp = shuffledArray[i];
       shuffledArray[i] = shuffledArray[randomIndex];
       shuffledArray[randomIndex] = temp;
     }
   
-    // Devuelve el array barajado
     return shuffledArray;
   }
 
+  function changeButtons(param) {
+    var borders = document.getElementsByClassName("border");;
+    for(var i = 0; i < allQuestions[0].options.length; i++) {
+      borders[i].setAttribute("data-disabled", param)
+    }
+  }
+
+
+  const esperar = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  };
 
   const getQuestions = async () => {
     try {
       const response = await axios.get(`${apiEndpoint}/gameUnlimitedQuestions`, { id });
       for (var i = 0; i < response.data.length; i++) {
-        var possibleAnswers = [response.data[i].respuesta_correcta, response.data[i].respuestas_incorrectas[0], response.data[i].respuestas_incorrectas[1], response.data[i].respuestas_incorrectas[2]]
+        var possibleAnswers = [
+          response.data[i].respuesta_correcta, 
+          response.data[i].respuestas_incorrectas[0], 
+          response.data[i].respuestas_incorrectas[1], 
+          response.data[i].respuestas_incorrectas[2]]
+
         possibleAnswers = shuffleArray(possibleAnswers)
+
         allQuestions.push({
           question: response.data[i].pregunta,
           options: possibleAnswers,
@@ -93,24 +98,14 @@ const Quiz = () => {
     } catch (error) {
       console.error(error);
     }
-};
-  function changeButtons(param) {
-    console.log("Entramos aqui")
-    var borders = document.getElementsByClassName("border");;
-    for(var i = 0; i < Math.min(borders.length, allQuestions[0].options.length); i++) {
-      borders[i].setAttribute("data-disabled", param)
-    }
-  }
-
+  };
+  
   const gameStore = async () => {
     try {
       var username = localStorage.getItem("username")
-      console.log(username)
-      console.log(questions)
       const response = await axios.post(`${apiEndpoint}/storeGame`, { id, username,  points, questions});
       questions = []
       points = 0
-      console.log(response)
     } catch (error) {
       console.error(error)
     }
@@ -126,7 +121,7 @@ const Quiz = () => {
     //Marcamos la respuesta correcta
     const numberAnswer = allQuestions[currentQuestionIndex].options.indexOf(allQuestions[currentQuestionIndex].correctAnswer)
     const botonCorrecta = document.getElementById('option-' + numberAnswer)
-    const previousBackgroundColor = '#1a1a1a'
+    const previousBackgroundColor = botonCorrecta.style.backgroundColor
     botonCorrecta.style.backgroundColor = 'green' 
     
     // Pasar a la siguiente pregunta después de responder
@@ -145,7 +140,6 @@ const Quiz = () => {
       currentQuestionIndex = (currentQuestionIndex + 1);
     }
     isCorrect = (false)
-    setRemTime(0)
         
     //Habilitamos botones
     changeButtons("false")
@@ -153,7 +147,6 @@ const Quiz = () => {
 
   const checkAnswer = async (option) => {
     getQuestions()
-    // console.log(option === questions[currentQuestionIndex].correctAnswer)
     isCorrect = (option === allQuestions[currentQuestionIndex].correctAnswer);
 
     changeButtons("true")
@@ -162,25 +155,13 @@ const Quiz = () => {
     const botonIncorrecta = document.getElementById('option-' + allQuestions[currentQuestionIndex].options.indexOf(option))
     const previousBackgroundColor = botonIncorrecta.style.backgroundColor
 
-    // console.log(haveFailedQuestion)
-    // console.log(isCorrect)
     if (!isCorrect) {
-      console.log("Entramos en el correct")
-      // console.log(isCorrect)
       botonIncorrecta.style.backgroundColor = 'red'
-      // console.log("Entramos a cambiar")
-      haveFailedQuestion = true;
-      // console.log("Despues de modificar los valores")
-
-
-      /*
-      Si queremos que la partida se corte cuando tenemos 3 fallos, descomentar: 
       
       setNumErrors(numErrors--);
       if(numErrors == 0){
         haveFailedQuestion = true;
       }
-      */
     } else {
       points = points += 100;
     }
@@ -205,18 +186,18 @@ const Quiz = () => {
       currentQuestionIndex = (currentQuestionIndex + 1);
     }
     isCorrect = (false)
-    setRemTime(0)
         
     
     changeButtons("false")
-    console.log(haveFailedQuestion)
     if(haveFailedQuestion) {
-      console.log("Entramos a guardar el juego")
       await gameStore()
       haveFailedQuestion = false;
       navigator('/menu')
     }
+
   };
+
+
 
   return (
     <>
@@ -260,9 +241,6 @@ const Quiz = () => {
 
         <GoBackButton/>
         
-        {/* {isCorrect !== null && (
-          <p>{isCorrect ? '¡Respuesta correcta!' : 'Respuesta incorrecta.'}</p>
-        )} */}
       </Container>
       <Footer />
     </>
