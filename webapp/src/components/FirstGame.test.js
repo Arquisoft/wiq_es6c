edc34c756075d6b2f5ba38267cdea3eacaf3f32f
Liliana <uo288574@uniovi.es>
2024-04-10 19:27:12 +0200

    imports

--- webapp/src/components/FirstGame.test.js
+++ webapp/src/components/FirstGame.test.js
@@ -1,4 +1,4 @@
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, act } from '@testing-library/react';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import {MemoryRouter} from 'react-router-dom';
