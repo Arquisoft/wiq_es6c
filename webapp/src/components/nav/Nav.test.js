bd0443f922e65dfaf1a401baad358e6c4c3e83f9
Liliana <uo288574@uniovi.es>
2024-04-22 08:42:03 +0200

    test to access to history

--- webapp/src/components/nav/Nav.test.js
+++ webapp/src/components/nav/Nav.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { ContextFun } from '../Context';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
@@ -25,6 +25,23 @@ describe("Nav component", () => {
 
         const linkElement = screen.getByText(/WIQ/i);
         expect(linkElement).toBeInTheDocument();
+    });
+
+    test("renders nav, access to history",async () => {
+
+        render(
+            <ContextFun>
+                <Router>
+                    <Nav/>
+                </Router>
+            </ContextFun>
+        );
+
+        const linkElement = screen.getByText(/Historial/i);
+        fireEvent.click(linkElement);
+
+        const textOnPage = screen.getByText(/Historial de/i);
+        expect(textOnPage).toBeInTheDocument();
     });
 
 });
