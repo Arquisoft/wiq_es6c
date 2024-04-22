63e22fd84fb96af8343487b711dd0627b45fb4c1
Liliana <uo288574@uniovi.es>
2024-04-22 08:55:26 +0200

    test to find text of Game Configuration

---
+++ webapp/src/components/game/GameConfiguration.test.js
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import { ContextFun } from '../Context';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { GameConfiguration } from './GameConfiguration';
+
+const mockAxios = new MockAdapter(axios);
+
+describe("Game Configuration", () => {
+
+    beforeEach(() => {
+        mockAxios.reset();
+    });
+
+    test("renders GameConfig",async () => {
+
+        render(
+            <ContextFun>
+                <Router>
+                    <GameConfiguration/>
+                </Router>
+            </ContextFun>
+        );
+
+        const linkElement = screen.getByText(/Configuración de la partida/i);
+        expect(linkElement).toBeInTheDocument();
+
+        linkElement = screen.getByText(/Selecciona las temáticas/i);
+        expect(linkElement).toBeInTheDocument();
+
+        linkElement = screen.getByText(/Selecciona el número de preguntas/i);
+        expect(linkElement).toBeInTheDocument();
+    });
+
+});
