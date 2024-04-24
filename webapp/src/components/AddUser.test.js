b7d598815b60caaac5a29b6cfb58a44ae2558e27
Liliana <uo288574@uniovi.es>
2024-04-24 20:01:55 +0200

    trying to reduce code duplication

--- webapp/src/components/AddUser.test.js
+++ webapp/src/components/AddUser.test.js
@@ -6,6 +6,13 @@ import AddUser from './AddUser';
 
 const mockAxios = new MockAdapter(axios);
 
+const nameInput = screen.getByLabelText(/Nombre/);
+const surnameInput = screen.getByLabelText(/Apellidos/i);
+const usernameInput = screen.getByLabelText(/Usuario/i);
+const passwordInput = screen.getAllByLabelText(/Contraseña/i)[0];
+const confirmPasswordInput = screen.getByLabelText(/Repetir contraseña/i);
+const addUserButton = document.getElementsByClassName('inner')[0]
+
 describe('AddUser component', () => {
   beforeEach(() => {
     mockAxios.reset();
@@ -13,13 +20,6 @@ describe('AddUser component', () => {
 
   it('should add user successfully', async () => {
     render(<AddUser />);
-
-    const nameInput = screen.getByLabelText(/Nombre/);
-    const surnameInput = screen.getByLabelText(/Apellidos/i);
-    const usernameInput = screen.getByLabelText(/Usuario/i);
-    const passwordInput = screen.getAllByLabelText(/Contraseña/i)[0];
-    const confirmPasswordInput = screen.getByLabelText(/Repetir contraseña/i);
-    const addUserButton = document.getElementsByClassName('inner')[0]
 
     // Mock the axios.post request to simulate a successful response
     mockAxios.onPost('http://localhost:8000/adduser').reply(200);
@@ -43,15 +43,6 @@ describe('AddUser component', () => {
   it('try to add user but not introduce name', async () => {
     render(<AddUser />);
 
-    const surnameInput = screen.getByLabelText(/Apellidos/i);
-    const usernameInput = screen.getByLabelText(/Usuario/i);
-    const passwordInput = screen.getAllByLabelText(/Contraseña/i)[0];
-    const confirmPasswordInput = screen.getByLabelText(/Repetir contraseña/i);
-    const addUserButton = document.getElementsByClassName('inner')[0]
-
-    // Mock the axios.post request to simulate a successful response
-    mockAxios.onPost('http://localhost:8000/adduser').reply(200);
-
     // Simulate user input
     fireEvent.change(surnameInput, { target: { value: 'testUser' } });
     fireEvent.change(usernameInput, { target: { value: 'testUser' } });
@@ -69,15 +60,6 @@ describe('AddUser component', () => {
 
   it('try to add user but not introduce surname', async () => {
     render(<AddUser />);
-
-    const nameInput = screen.getByLabelText(/Nombre/);
-    const usernameInput = screen.getByLabelText(/Usuario/i);
-    const passwordInput = screen.getAllByLabelText(/Contraseña/i)[0];
-    const confirmPasswordInput = screen.getByLabelText(/Repetir contraseña/i);
-    const addUserButton = document.getElementsByClassName('inner')[0]
-
-    // Mock the axios.post request to simulate a successful response
-    mockAxios.onPost('http://localhost:8000/adduser').reply(200);
 
     // Simulate user input
     fireEvent.change(nameInput, { target: { value: 'testUser' } });
@@ -97,16 +79,6 @@ describe('AddUser component', () => {
   it('try to add user but different passwords', async () => {
     render(<AddUser />);
 
-    const nameInput = screen.getByLabelText(/Nombre/);
-    const surnameInput = screen.getByLabelText(/Apellidos/i);
-    const usernameInput = screen.getByLabelText(/Usuario/i);
-    const passwordInput = screen.getAllByLabelText(/Contraseña/i)[0];
-    const confirmPasswordInput = screen.getByLabelText(/Repetir contraseña/i);
-    const addUserButton = document.getElementsByClassName('inner')[0]
-
-    // Mock the axios.post request to simulate a successful response
-    mockAxios.onPost('http://localhost:8000/adduser').reply(200);
-
     // Simulate user input
     fireEvent.change(nameInput, { target: { value: 'testUser' } });
     fireEvent.change(surnameInput, { target: { value: 'testUser' } });
@@ -125,13 +97,6 @@ describe('AddUser component', () => {
 
   it('should handle error when adding user', async () => {
     render(<AddUser />);
-
-    const nameInput = screen.getByLabelText(/Nombre/);
-    const surnameInput = screen.getByLabelText(/Apellidos/i);
-    const usernameInput = screen.getByLabelText(/Usuario/i);
-    const passwordInput = screen.getAllByLabelText(/Contraseña/i)[0];
-    const confirmPasswordInput = screen.getByLabelText(/Repetir contraseña/i);
-    const addUserButton = document.getElementsByClassName('inner')[0]
 
     // Mock the axios.post request to simulate an error response
     mockAxios.onPost('http://localhost:8000/adduser').reply(500, { error: 'Internal Server Error' });
