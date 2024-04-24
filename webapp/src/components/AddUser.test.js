1eea74375a1fef1ea8f052cc852f42873534e4f6
Liliana <uo288574@uniovi.es>
2024-04-24 20:13:11 +0200

    reduce code

--- webapp/src/components/AddUser.test.js
+++ webapp/src/components/AddUser.test.js
@@ -54,55 +54,18 @@ describe('AddUser component', () => {
     });
   });
 
-  it('try to add user but not introduce surname', async () => {
-    render(<AddUser />);
-
-    const nameInput = screen.getByLabelText(/Nombre/);
-    const usernameInput = screen.getByLabelText(/Usuario/i);
-    const passwordInput = screen.getAllByLabelText(/Contraseña/i)[0];
-    const confirmPasswordInput = screen.getByLabelText(/Repetir contraseña/i);
-    const addUserButton = document.getElementsByClassName('inner')[0]
-
-    // Mock the axios.post request to simulate a successful response
-    mockAxios.onPost('http://localhost:8000/adduser').reply(200);
-
-    // Simulate user input
-    fireEvent.change(nameInput, { target: { value: 'testUser' } });
-    fireEvent.change(usernameInput, { target: { value: 'testUser' } });
-    fireEvent.change(passwordInput, { target: { value: 'testPassword' } });
-    fireEvent.change(confirmPasswordInput, { target: { value: 'password' } });
-
-    // Trigger the add user button click
-    fireEvent.click(addUserButton);
-
-    // Wait for the Snackbar to be open
-    await waitFor(() => {
-      expect(screen.getByText(/Por favor, introduzca tanto el nombre como los apellidos./i)).toBeInTheDocument();
-    });
-  });
-
   it('try to add user but different passwords', async () => {
     render(<AddUser />);
 
-    const nameInput = screen.getByLabelText(/Nombre/);
-    const surnameInput = screen.getByLabelText(/Apellidos/i);
-    const usernameInput = screen.getByLabelText(/Usuario/i);
-    const passwordInput = screen.getAllByLabelText(/Contraseña/i)[0];
-    const confirmPasswordInput = screen.getByLabelText(/Repetir contraseña/i);
-    const addUserButton = document.getElementsByClassName('inner')[0]
-
-    // Mock the axios.post request to simulate a successful response
-    mockAxios.onPost('http://localhost:8000/adduser').reply(200);
-
     // Simulate user input
-    fireEvent.change(nameInput, { target: { value: 'testUser' } });
-    fireEvent.change(surnameInput, { target: { value: 'testUser' } });
-    fireEvent.change(usernameInput, { target: { value: 'testUser' } });
-    fireEvent.change(passwordInput, { target: { value: 'testPassword' } });
-    fireEvent.change(confirmPasswordInput, { target: { value: 'password' } });
+    fireEvent.change(screen.getByLabelText(/Nombre/), { target: { value: 'userForTest' } });
+    fireEvent.change(screen.getByLabelText(/Apellidos/i), { target: { value: 'userForTest' } });
+    fireEvent.change(screen.getByLabelText(/Usuario/i), { target: { value: 'userForTest' } });
+    fireEvent.change(screen.getAllByLabelText(/Contraseña/i)[0], { target: { value: 'testPassword' } });
+    fireEvent.change(screen.getByLabelText(/Repetir contraseña/i), { target: { value: 'password' } });
 
     // Trigger the add user button click
-    fireEvent.click(addUserButton);
+    fireEvent.click(document.getElementsByClassName('inner')[0]);
 
     // Wait for the Snackbar to be open
     await waitFor(() => {
@@ -112,26 +75,19 @@ describe('AddUser component', () => {
 
   it('should handle error when adding user', async () => {
     render(<AddUser />);
-
-    const nameInput = screen.getByLabelText(/Nombre/);
-    const surnameInput = screen.getByLabelText(/Apellidos/i);
-    const usernameInput = screen.getByLabelText(/Usuario/i);
-    const passwordInput = screen.getAllByLabelText(/Contraseña/i)[0];
-    const confirmPasswordInput = screen.getByLabelText(/Repetir contraseña/i);
-    const addUserButton = document.getElementsByClassName('inner')[0]
-
+    
     // Mock the axios.post request to simulate an error response
     mockAxios.onPost('http://localhost:8000/adduser').reply(500, { error: 'Internal Server Error' });
 
     // Simulate user input
-    fireEvent.change(nameInput, { target: { value: 'testUser' } });
-    fireEvent.change(surnameInput, { target: { value: 'testUser' } });
-    fireEvent.change(usernameInput, { target: { value: 'testUser' } });
-    fireEvent.change(passwordInput, { target: { value: 'testPassword' } });
-    fireEvent.change(confirmPasswordInput, { target: { value: 'testPassword' } });
+    fireEvent.change(screen.getByLabelText(/Nombre/), { target: { value: 'testUser' } });
+    fireEvent.change(screen.getByLabelText(/Apellidos/i), { target: { value: 'testUser' } });
+    fireEvent.change(screen.getByLabelText(/Usuario/i), { target: { value: 'testUser' } });
+    fireEvent.change(screen.getAllByLabelText(/Contraseña/i)[0], { target: { value: 'testPassword' } });
+    fireEvent.change(screen.getByLabelText(/Repetir contraseña/i), { target: { value: 'testPassword' } });
 
     // Trigger the add user button click
-    fireEvent.click(addUserButton);
+    fireEvent.click(document.getElementsByClassName('inner')[0]);
 
     // Wait for the error Snackbar to be open
     await waitFor(() => {
