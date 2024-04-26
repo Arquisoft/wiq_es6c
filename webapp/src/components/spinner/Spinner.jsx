43f93c06032ee6c1ffd508075affb22a02e8178b
Marco Quintana <UO287872@uniovi.es>
2024-04-26 16:52:52 +0200

    Añadido spinner

---
+++ webapp/src/components/spinner/Spinner.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import './Spinner.css';
+
+class Spinner extends React.Component {
+	state = {
+		value: this.props.value || 0
+	}
+	
+	constructor(props) {
+		super(props);
+		
+		this.increment = this.increment.bind(this);
+		this.decrement = this.decrement.bind(this);
+	}
+	
+	get value() {
+	  return this.state.value;
+	}
+  
+	increment() {
+	  const { max } = this.props;
+	  
+	  if (typeof max === 'number' && this.value >= max) return;
+	  
+	  this.setState({ value: this.value + 1 });
+	}
+  
+	decrement() {
+	  const { min } = this.props;
+	  
+	  if (typeof min === 'number' && this.value <= min) return;
+	  
+	  this.setState({ value: this.value - 1 });
+	}
+	
+	render() {
+	  return (
+		<div className="input-number">
+		  <button type="button" onClick={this.decrement}>&minus;</button>
+		  <span>{this.value}</span>
+		  <button type="button" onClick={this.increment}>&#43;</button>     
+		</div>
+	  )
+	}
+  }
+
+export default Spinner;
